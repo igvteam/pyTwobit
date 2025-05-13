b3ec335021eebe889a41ff84647f4d8af2e35303
jrobinso <933148+jrobinso@users.noreply.github.com>
2025-05-13 16:32:37 -0700

    normalized project name

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-
 from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='pyTwobit',
+    name='pytwobit',  # Normalized project name
     version='0.3.0',
     description='A fast reader for local or remote UCSC twobit sequence files.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
     author='Jim Robinson',
-    url='https://github.com/jrobinso/pyTwobit',
-    packages=['pytwobit'],
-    package_data={'pytwobit': ['tests/foo.2bit']},
+    url='https://github.com/jrobinso/pytwobit',
+    packages=['pytwobit'],  # Ensure this matches the normalized name
+    package_data={'pytwobit': ['tests/foo.2bit']},  # Match normalized name
     install_requires=[
         'requests',
     ],
